dafe3b66d875c9fec7fa13c8632d65e80d9c97d8
unknown <amienulrana@gmail.com>
2023-12-20 09:59:45 +0700

    fix erro build in production

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaWhatsapp, FaWhatsappSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useLanguageContext } from "@/context/language-context";
@@ -128,6 +128,13 @@ export default function Intro() {
           target="_blank"
         >
           <FaGithubSquare />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://wa.me/6285260298204?text=Hello Amienul"
+          target="_blank"
+        >
+          <FaWhatsappSquare />
         </a>
       </motion.div>
     </section>
