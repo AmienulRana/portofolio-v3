da74dc848b022ee511af3b0fb4a3f6a8bbf5844c
unknown <amienulrana@gmail.com>
2024-02-04 08:39:18 +0700

    change url

--- app/callyuk/page.tsx
+++ app/callyuk/page.tsx
@@ -4,18 +4,18 @@ import { useEffect } from "react";
 
 export default function CallYuk() {
   useEffect(() => {
-    // setTimeout(() => {
-    //   window.location.href =
-    //     "https://70f5-2001-448a-10c0-36f1-85a8-90f2-fa57-fb88.ngrok-free.app";
-    // }, 3000);
+    setTimeout(() => {
+      window.location.href =
+        "https://6a3b-2001-448a-10c0-36f1-b1fb-396a-a599-9d7e.ngrok-free.app";
+    }, 3000);
   }, []);
   return (
     <div className="w-full flex z-[999] flex-col items-center justify-center fixed top-0 left-0 min-h-screen bg-white">
       <h1 className="text-[36px] text-[#0060FF] font-bold">CallYuk</h1>
       <p className="text-gray-400 mb-5">
-        Saat ini server callyuk sedang mati.
+        Anda akan diredirect ke dalam aplikasi dalam beberapa saat
       </p>
-      {/* <span className="loader"></span> */}
+      <span className="loader"></span>
     </div>
   );
 }
